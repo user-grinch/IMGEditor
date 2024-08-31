bfa98e88953d0995c2c81acf2a5f26e26f5e58c6
Grinch_ <user.grinch@gmail.com>
2024-08-31 15:06:00 +0600

    up version ->0.5-beta

--- tools/Setup.bat
+++ tools/Setup.bat
@@ -1,10 +1,28 @@
-rem Generate visual studio files and run the devcmd init
 @echo off
+set "projectdir=%CD%"
 
-rem Set game paths below
-set "OUT_DIR="E:\GTASanAndreas""
+:find_vs_path
+set "vs_path="
+for %%d in (Community Preview Professional) do (
+    for /d %%e in ("C:\Program Files\Microsoft Visual Studio\2022\%%d") do (
+        if exist "%%e\Common7\Tools\VsDevCmd.bat" (
+            set "vs_path=%%e"
+            goto :vs_install_found
+        )
+    )
+)
+goto :eof
 
-cd tools
+:vs_install_found
+if not defined vs_path (
+    echo Error: Visual Studio installation not found.
+    exit /b 1
+)
+
+:run_premake
+cd "%projectdir%\tools"
 premake5.exe vs2022
-cd ../build
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+
+:run_dev
+cd "%projectdir%\build"
+call %vs_path%"\Common7\Tools\VsDevCmd.bat"
